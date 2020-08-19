530072bf12a701c5a7bfb44628b174787d1f4e7f
wuzhenxi520 <wuxi1398524837@live.com>
2020-08-19 22:48:49 +0800

    根据ip获取详细信息捕获报错

--- src/main/java/com/wzx/util/RequestUtils.java
+++ src/main/java/com/wzx/util/RequestUtils.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -40,23 +41,23 @@ public class RequestUtils {
 
     public String getRequestIpOnly(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
 
@@ -65,15 +66,15 @@ public class RequestUtils {
 
     public String getRequestIp(HttpServletRequest request) throws UnknownHostException {
         String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
 
-        if (ip == null || ip.length() == 0 || "unknown" .equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
             if (ip.equals("127.0.0.1")) {
                 InetAddress inetAddress = InetAddress.getLocalHost();
@@ -85,7 +86,7 @@ public class RequestUtils {
             ip = ip.substring(0, ip.indexOf(","));
         }
 
-        return "0:0:0:0:0:0:0:1" .equals(ip) ? "127.0.0.1" : ip;
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
     }
 
     public IPDataDTO queryLocationByIpJSON(String ip) {
@@ -111,11 +112,14 @@ public class RequestUtils {
 
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params,
                 headers);
-        ResponseEntity<IPDataDTO> dataDTOResponseEntity = restTemplate
-                .exchange(queryIpUri, HttpMethod.POST, requestEntity, IPDataDTO.class);
-
-        if (dataDTOResponseEntity.getStatusCode().equals(HttpStatus.OK)) {
-            ipDataDTO = dataDTOResponseEntity.getBody();
+        try {
+            ResponseEntity<IPDataDTO> dataDTOResponseEntity = restTemplate
+                    .exchange(queryIpUri, HttpMethod.POST, requestEntity, IPDataDTO.class);
+            if (dataDTOResponseEntity.getStatusCode().equals(HttpStatus.OK)) {
+                ipDataDTO = dataDTOResponseEntity.getBody();
+            }
+        } catch (RestClientException e) {
+            log.error("根据ip获取详细信息报错.Err:{},Exception:{}", e.getMessage(), e);
         }
         return ipDataDTO;
     }
